d2ea36b1ec332c96ff55058b3973367601079e9a
Pascal Boutin <pboutin@internap.com>
2017-01-03 16:23:05 -0500

    Update 2017-01-03T21:23:03.555Z

--- docs.js
+++ docs.js
@@ -1,8 +1,6 @@
 hljs.initHighlightingOnLoad();
 
 $(document).ready(function() {
-
-
     $('body > nav button[data-flavor]').on('click', function() {
         var $linkTag = $('head link#main');
         var $this = $(this);
